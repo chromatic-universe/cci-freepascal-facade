85a5e16f0a51469f392827e05fcf80fb24840c90
chromatic-universe <chromaticuniverse79@gmail.com>
2019-03-21 17:32:16 -0400

    message data

--- kafka-gate/KafkaPas/frrm_message_lst.pas
+++ kafka-gate/KafkaPas/frrm_message_lst.pas
@@ -16,6 +16,9 @@ type
     stream_messages: TListBox;
     PopupMenu1: TPopupMenu;
     procedure stream_messagesClick(Sender: TObject);
+    procedure stream_messagesMouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure stream_messagesShowHint(Sender: TObject; HintInfo: PHintInfo);
   private
     { private declarations }
   public
@@ -32,9 +35,37 @@ implementation
 { Tfrm_message_lst }
 
 procedure Tfrm_message_lst.stream_messagesClick(Sender: TObject);
+var
+  out : string;
+  jdata : TJSONData;
 begin
-     if stream_messages.ItemIndex > -1 then
-        ShowMessage( GetJson( stream_messages.Items[stream_messages.ItemIndex] ).FormatJSON() );
+     //if stream_messages.ItemIndex > -1 then
+       //ShowMessage( stream_messages.Items[stream_messages.ItemIndex] );
+    //   out := stream_messages.Items[stream_messages.ItemIndex];
+    // ShowMessage( out );
+     //jdata :=  GetJson( out);
+     //dw :=
+end;
+
+procedure Tfrm_message_lst.stream_messagesMouseMove(Sender: TObject;
+  Shift: TShiftState; X, Y: Integer);
+var
+  k : integer;
+begin
+    k := stream_messages.ItemAtPos( Point( X , Y ) , true );
+    if k = -1 then begin
+      stream_messages.ShowHint := false;
+    end else begin
+      stream_messages.ShowHint := true;
+      stream_messages.hint := stream_messages.Items[k];
+    end;
+end;
+
+procedure Tfrm_message_lst.stream_messagesShowHint(Sender: TObject;
+  HintInfo: PHintInfo);
+
+begin
+
 end;
 
 end.
