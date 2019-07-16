b9954b65a2e85c7ca2c72dd2b5858fa6c8d1cf62
chromatic-universe <chromaticuniverse79@gmail.com>
2019-07-16 14:41:46 -0400

    afternoon

---
+++ kafka-gate/KafkaPas/frm_post_up_one_partition.pas
@@ -0,0 +1,26 @@
+unit frm_post_up_one_partition;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  Tform_post_up_one_partition = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  form_post_up_one_partition: Tform_post_up_one_partition;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
