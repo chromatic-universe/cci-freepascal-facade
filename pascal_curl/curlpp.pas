280d94426e8addbcd8897d44c8e41d966af82a39
chromatic-universe <chromaticuniverse79@gmail.com>
2021-03-31 12:37:13 -0400

    pascal_curl

---
+++ pascal_curl/curlpp.pas
@@ -0,0 +1,94 @@
+unit curlpp;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,libpascurl;
+
+type
+
+  { Tfrm_pascal_run }
+
+  Tfrm_pascal_run = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+    frm_pascal_run: Tfrm_pascal_run;
+    buffer : TStringStream;
+    h_curl : CURL;
+    effectiveUrl, contentType, ip : PChar;
+    responseCode, headerSize : Longint;
+    contentLength, totalTime : Longword;
+    ret :CURLcode;
+
+implementation
+
+{$R *.lfm}
+
+{ Tfrm_pascal_run }
+function WriteFunctionCallback (ptr : PChar; size : LongWord;
+    nmemb : LongWord; data : Pointer)  : integer;
+begin
+    buffer.WriteString(string(ptr));
+    //writeln( buffer.DataString );
+    result := size * nmemb;
+end;
+
+procedure Tfrm_pascal_run.Button1Click(Sender: TObject);
+var
+    fs : TFileStream;
+begin
+      curl_global_init(CURL_GLOBAL_DEFAULT);
+      h_curl := curl_easy_init();
+      curl_easy_setopt( h_curl , CURLOPT_URL, PChar('https://illwill.com/no-future') );
+
+      buffer := TStringStream.Create('');
+      fs := TFileStream.Create( '/home/wiljoh/foo.html', fmCreate );
+      curl_easy_setopt( h_curl , CURLOPT_WRITEFUNCTION, @WriteFunctionCallback);
+      //curl_easy_setopt( h_curl , CURLOPT_WRITEDATA, buffer);
+
+        try
+            ret  :=  curl_easy_perform( h_curl );
+            if ret = CURLE_OK then
+            begin
+              New(effectiveUrl);
+              New(contentType);
+              New(ip);
+
+              curl_easy_getinfo(h_curl, CURLINFO_EFFECTIVE_URL, @effectiveUrl);
+              curl_easy_getinfo(h_curl, CURLINFO_RESPONSE_CODE, @responseCode);
+              curl_easy_getinfo(h_curl, CURLINFO_HEADER_SIZE, @headerSize);
+              curl_easy_getinfo(h_curl, CURLINFO_CONTENT_TYPE, @contentType);
+              curl_easy_getinfo(h_curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, @contentLength);
+              curl_easy_getinfo(h_curl, CURLINFO_LOCAL_IP, @ip);
+              curl_easy_getinfo(h_curl, CURLINFO_TOTAL_TIME_T, @totalTime);
+
+              writeln('URL: ':20,                 effectiveUrl);
+              writeln('Response code: ':20,       responseCode);
+              writeln('Header size, kB: ':20,     FormatFloat('0.00', headerSize / 1024));
+              writeln('Content type: ',           contentType);
+              writeln('Content length, kB: ':20,  FormatFloat('0.00', contentLength / 1024));
+              writeln('IP: ':20,                  ip);
+              writeln('Total time, ms: ':20,      totalTime);
+              writeln('==== Content ====');
+              writeln(buffer.DataString);
+              fs.WriteBuffer(Pointer(buffer.DataString)^, Length(buffer.DataString));
+            end;
+        finally
+          buffer.free;
+          fs.free;
+        end;
+
+      curl_global_cleanup;
+end;
+
+end.
+
---
+++ pascal_curl/pascal_curl.lpr
@@ -0,0 +1,22 @@
+program pascal_curl;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, curlpp
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(Tfrm_pascal_run, frm_pascal_run);
+  Application.Run;
+end.
+
