127875c2d5b3bc87e165f162f597feb1b43f2aec
chromatic-universe <chromaticuniverse79@gmail.com>
2021-04-01 02:32:59 -0400

    pascal_curl

--- pascal_curl/backup/curlpp.pas
+++ pascal_curl/backup/curlpp.pas
@@ -5,13 +5,14 @@ unit curlpp;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,libpascurl;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,libpascurl,
+  fpjson,cci_curl_pascal;
 
 type
 
-  { TForm1 }
+  { Tfrm_pascal_run }
 
-  TForm1 = class(TForm)
+  Tfrm_pascal_run = class(TForm)
     Button1: TButton;
     procedure Button1Click(Sender: TObject);
   private
@@ -20,8 +21,9 @@ type
 
   end;
 
+
 var
-    Form1: TForm1;
+    frm_pascal_run: Tfrm_pascal_run;
     buffer : TStringStream;
     h_curl : CURL;
     effectiveUrl, contentType, ip : PChar;
@@ -33,61 +35,97 @@ implementation
 
 {$R *.lfm}
 
-{ TForm1 }
-function WriteFunctionCallback (ptr : PChar; size : LongWord;
-    nmemb : LongWord; data : Pointer)  : integer;
+{ Tfrm_pascal_run }
+
+
+function write_function_callback( ptr : PChar; size : LongWord;
+    nmemb : LongWord; data : Pointer )  : integer;
+var
+    strm : TStringStream;
 begin
-    buffer.WriteString(string(ptr));
-    //writeln( buffer.DataString );
-    result := size * nmemb;
+          //strm := TSTringStream.Create();
+          strm :=  TStringStream( data^ );
+           if Assigned( strm ) then
+           begin
+                strm.WriteString( string( ptr ) );
+           end;
+          result := size * nmemb;
 end;
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure Tfrm_pascal_run.Button1Click(Sender: TObject);
 var
-    fs : TFileStream;
+
+    O : TJSONObject;
+    curl_header_lst :  pcurl_slist;
+
 begin
-      curl_global_init(CURL_GLOBAL_DEFAULT);
-      h_curl := curl_easy_init();
-      curl_easy_setopt( h_curl , CURLOPT_URL, PChar('https://illwill.com/no-future') );
+          //
+          //construct request
+          //
+          curl_header_lst := nil;
+          //init curl environment
+          curl_global_init( CURL_GLOBAL_DEFAULT);
+          //init local handle
+          h_curl := curl_easy_init();
+          //headers  data
+          curl_header_lst := curl_slist_append( curl_header_lst ,  PChar( 'Content-Type: application/json' ) );
+          curl_easy_setopt( h_curl, CURLOPT_HTTPHEADER , curl_header_lst );
+          //post data
+          O := TJSONObject.Create( ['user','william.kevin.johnson','password','Argentina1'] );
+          curl_easy_setopt( h_curl, CURLOPT_POSTFIELDS , PChar( O.FormatJson ) );
+          //destination url
+          curl_easy_setopt( h_curl , CURLOPT_URL, PChar('https://localhost:7080/mongo/imap2017/plain_text_auth') );
+          curl_easy_setopt( h_curl ,  CURLOPT_FAILONERROR , 1 );
+          //set write callback
+          curl_easy_setopt( h_curl , CURLOPT_WRITEFUNCTION, @write_function_callback );
+          //write callback elastic buffer
+          buffer := TStringStream.Create( '' );
+          curl_easy_setopt( h_curl , CURLOPT_WRITEDATA, @buffer );
+          //skip peer verification
+          curl_easy_setopt( h_curl , CURLOPT_SSL_VERIFYPEER, 0 );
+          //skip host verification
+          curl_easy_setopt( h_curl , CURLOPT_SSL_VERIFYHOST, 0 );
+          //debug
+          //curl_easy_setopt( h_curl, CURLOPT_DEBUGFUNCTION, @debug_trace_callback );
+          //
+          //perform
+          //
+          try
+                ret  :=  curl_easy_perform( h_curl );
+                if ret = CURLE_OK then
+                begin
+                  New(effectiveUrl);
+                  New(contentType);
+                  New(ip);
+                  //
+                  //out
+                  //
+                  curl_easy_getinfo(h_curl, CURLINFO_EFFECTIVE_URL, @effectiveUrl);
+                  curl_easy_getinfo(h_curl, CURLINFO_RESPONSE_CODE, @responseCode);
+                  curl_easy_getinfo(h_curl, CURLINFO_HEADER_SIZE, @headerSize);
+                  curl_easy_getinfo(h_curl, CURLINFO_CONTENT_TYPE, @contentType);
+                  curl_easy_getinfo(h_curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, @contentLength);
+                  curl_easy_getinfo(h_curl, CURLINFO_LOCAL_IP, @ip);
+                  curl_easy_getinfo(h_curl, CURLINFO_TOTAL_TIME_T, @totalTime);
 
-      buffer := TStringStream.Create('');
-      fs := TFileStream.Create( '/home/wiljoh/foo.html', fmCreate );
-      curl_easy_setopt( h_curl , CURLOPT_WRITEFUNCTION, @WriteFunctionCallback);
-      //curl_easy_setopt( h_curl , CURLOPT_WRITEDATA, buffer);
+                  writeln('URL: ':20,                 effectiveUrl);
+                  writeln('Response code: ':20,       responseCode);
+                  writeln('Header size, kB: ':20,     FormatFloat('0.00', headerSize / 1024));
+                  writeln('Content type: ',           contentType);
+                  writeln('Content length, kB: ':20,  FormatFloat('0.00', contentLength / 1024));
+                  writeln('IP: ':20,                  ip);
+                  writeln('Total time, ms: ':20,      totalTime);
+                  writeln('==== Content ====');
+                  writeln(buffer.DataString);
 
-        try
-            ret  :=  curl_easy_perform( h_curl );
-            if ret = CURLE_OK then
-            begin
-              New(effectiveUrl);
-              New(contentType);
-              New(ip);
+                end;
+          finally
+              buffer.free;
+              curl_slist_free_all( curl_header_lst );
+              curl_global_cleanup;
+          end;
 
-              curl_easy_getinfo(h_curl, CURLINFO_EFFECTIVE_URL, @effectiveUrl);
-              curl_easy_getinfo(h_curl, CURLINFO_RESPONSE_CODE, @responseCode);
-              curl_easy_getinfo(h_curl, CURLINFO_HEADER_SIZE, @headerSize);
-              curl_easy_getinfo(h_curl, CURLINFO_CONTENT_TYPE, @contentType);
-              curl_easy_getinfo(h_curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, @contentLength);
-              curl_easy_getinfo(h_curl, CURLINFO_LOCAL_IP, @ip);
-              curl_easy_getinfo(h_curl, CURLINFO_TOTAL_TIME_T, @totalTime);
 
-              writeln('URL: ':20,                 effectiveUrl);
-              writeln('Response code: ':20,       responseCode);
-              writeln('Header size, kB: ':20,     FormatFloat('0.00', headerSize / 1024));
-              writeln('Content type: ',           contentType);
-              writeln('Content length, kB: ':20,  FormatFloat('0.00', contentLength / 1024));
-              writeln('IP: ':20,                  ip);
-              writeln('Total time, ms: ':20,      totalTime);
-              writeln('==== Content ====');
-              writeln(buffer.DataString);
-              fs.WriteBuffer(Pointer(buffer.DataString)^, Length(buffer.DataString));
-            end;
-        finally
-          buffer.free;
-          fs.free;
-        end;
-
-      curl_global_cleanup;
 end;
 
 end.
--- pascal_curl/pascal_curl.lpr
+++ pascal_curl/pascal_curl.lpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, curlpp
+  Forms, curlpp, cci_curl_pascal
   { you can add units after this };
 
 {$R *.res}
